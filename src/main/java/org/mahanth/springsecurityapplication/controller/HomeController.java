d7917f87743bc46eb8405573e1b4a2588a695a44
SaiMahanth-Source <saimahanthnagendla3@gmail.com>
2026-05-01 21:28:50 +0530

    Sending status code along with the response and implemented login security featur using spring security

---
+++ src/main/java/org/mahanth/springsecurityapplication/controller/HomeController.java
@@ -0,0 +1,22 @@
+package org.mahanth.springsecurityapplication.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    /*
+    http://localhost:8080/login?logout for logout form and also spring maintains the session as well
+    httpServletRequest.getSession().getId() to get the session Id generated
+     */
+    @GetMapping("/")
+    public ResponseEntity<String> greet(HttpServletRequest httpServletRequest){
+
+        return new ResponseEntity<>("Welcome to the home page ", HttpStatus.OK);
+//        return new ResponseEntity<>("Welcome to the home page " + httpServletRequest.getSession().getId(), HttpStatus.OK);
+    }
+}
