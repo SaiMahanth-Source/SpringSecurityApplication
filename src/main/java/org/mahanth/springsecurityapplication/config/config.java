19a2e7309cab2415487eb09fdeda4dc7ca65d495
SaiMahanth-Source <saimahanthnagendla3@gmail.com>
2026-05-08 00:17:53 +0530

    Implemented Verify user feature usig credentials from database

--- src/main/java/org/mahanth/springsecurityapplication/config/config.java
+++ src/main/java/org/mahanth/springsecurityapplication/config/config.java
@@ -1,7 +1,10 @@
 package org.mahanth.springsecurityapplication.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,12 +12,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class config {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -62,4 +69,13 @@ public class config {
 //
 //        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
 //    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+
+        return daoAuthenticationProvider;
+    }
 }
---
+++ src/main/java/org/mahanth/springsecurityapplication/service/MyUserDetailService.java
@@ -0,0 +1,30 @@
+package org.mahanth.springsecurityapplication.service;
+
+import org.mahanth.springsecurityapplication.model.User;
+import org.mahanth.springsecurityapplication.model.UserPrinciple;
+import org.mahanth.springsecurityapplication.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepo.findByUserName(username);
+
+        if(user == null) {
+            throw new UsernameNotFoundException("User Not Found" + username);
+        }
+
+        System.out.println(user);
+        return new UserPrinciple(user);
+    }
+}
